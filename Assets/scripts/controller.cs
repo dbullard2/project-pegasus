082be967f5386a7006b88e4c21ccc49726ed5614
dbullard2 <spacegeek84@gmail.com>
2019-06-11 23:37:15 -0500

    initial commit

---
+++ Assets/scripts/controller.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+    void Start() {
+
+    }
+
+    void FixedUpdate() {
+      if(Input.GetKey("right")){
+        Debug.Log("hi");
+        transform.Translate(Vector3.right * Time.deltaTime);
+      }
+
+      if(Input.GetKey("left")){
+        Debug.Log("bye");
+        transform.Translate(Vector3.left * Time.deltaTime);
+      }
+    }
+}
